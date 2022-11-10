ab9321fef35e859eea9cb969571cdb8068f5abd4
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2022-11-10 12:10:20 +0530

    Prepare UI skeleton

---
+++ app/src/main/java/com/anvaishy/easytmedc_receptionist_app/DoctorListFragment.java
@@ -0,0 +1,36 @@
+package com.anvaishy.easytmedc_receptionist_app;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DoctorListFragment extends Fragment {
+
+    private DoctorListViewModel mViewModel;
+
+    public static DoctorListFragment newInstance() {
+        return new DoctorListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_doctor_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DoctorListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/anvaishy/easytmedc_receptionist_app/MedDocsFragment.java
@@ -0,0 +1,36 @@
+package com.anvaishy.easytmedc_receptionist_app;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MedDocsFragment extends Fragment {
+
+    private MedDocsViewModel mViewModel;
+
+    public static MedDocsFragment newInstance() {
+        return new MedDocsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_med_docs, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MedDocsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
